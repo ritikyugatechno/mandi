6062139eb8c02a225b4bdac74d671a02a91e83fa
itzmeranjeet <ranjeetchauhan212000@gmail.com>
2024-06-17 11:46:20 +0530

    16-06-2024 "RANJEET"

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,15 +1,75 @@
-// src/pages/pdf/index.tsx
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#E4E4E4",
+    padding: 10,
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+  heading: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 12,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  column: {
+    flexDirection: "column",
+    flex: 1,
+    margin: 5,
+  },
+});
 
-const PdfPage: React.FC = () => {
+// Create Document Component
+const PdfDocument = () => {
+  const [showData, setShowData] = useState([]);
 
+  useEffect(() => {
+    fetch("/api/dashboard/get-data-by-date")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("useEffect", data);
+        setShowData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
-    <div className="container mx-auto py-10">
-      <button>Print</button>
-    </div>
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          {/* <Text style={styles.heading}>Supplier Names</Text> */}
+          {showData.map((data, index) => (
+            <Text key={index} style={styles.text}>
+              {data.supplierName[0]}
+            </Text>
+          ))}
+        </View>
+        <View style={styles.section}>
+          {/* <Text style={styles.heading}>Farmer Names</Text> */}
+          {showData.map((data, index) => (
+            <Text key={index} style={styles.text}>
+              {data.farmerName[0]}
+            </Text>
+          ))}
+        </View>
+      </Page>
+    </Document>
   );
 };
 
-export default PdfPage;
+export default PdfDocument;

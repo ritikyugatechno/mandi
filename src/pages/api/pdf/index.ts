149b98800384b676f3f62c78db6f01272b87a902
ritikprincemali <ritikprincemali@gmail.com>
2024-06-19 13:50:14 +0530

    api pdf

--- src/pages/api/pdf/index.ts
+++ src/pages/api/pdf/index.ts
@@ -11,7 +11,7 @@ export default async function handler(
     const data = await prisma.formData.findMany({
         
     });
-    console.log(data);
+    return res.status(200).json(data)
   }
 
   return res.status(200).json({ success: true });

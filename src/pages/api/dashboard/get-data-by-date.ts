e3233f533247675022c460613709a75c9544105a
ritikprincemali <ritikprincemali@gmail.com>
2024-06-19 12:58:17 +0530

    18/06/2024

--- src/pages/api/dashboard/get-data-by-date.ts
+++ src/pages/api/dashboard/get-data-by-date.ts
@@ -1,8 +1,6 @@
 
+import { prisma } from "@/lib/prismaClient";
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
--- src/pages/api/dashboard/index.ts
+++ src/pages/api/dashboard/index.ts
@@ -1,7 +1,5 @@
+import { prisma } from "@/lib/prismaClient";
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,

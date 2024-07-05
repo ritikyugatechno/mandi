a447e940da48c4bd1b4a92f04818fc09a60f27c7
ritikyugatechno <ritikyugatechno@gmail.com>
2024-07-05 17:47:09 +0530

    05 Fri

--- src/app/entry/page.tsx
+++ src/app/entry/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { fieldType, formData, formName } from "./form/formData";
-import { Input } from "@/components/ui/input";
 import {
   Command,
   CommandGroup,
@@ -8,6 +6,7 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -17,28 +16,28 @@ import {
 } from "@/components/ui/select";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
+import { fieldType, formData, formName, NextSelect } from "./form/formData";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 import React, { useEffect, useRef, useState } from "react";
-import { submitFormData, useFetchListOfNames } from "./data/query";
+import { formSubmit } from "../dashboard/formSubmit";
+import { updateEntry } from "./data/entrySlice";
 import { useAppDispatch, useAppSelector } from "./data/hooks";
-import { resetEntry, updateEntry } from "./data/entrySlice";
-import { Label } from "@/components/ui/label";
-import { addWeight, resetWeight, updataWeight } from "./data/weightSlice";
-import { toast } from "sonner";
-import { formSubmit } from "./form/formSubmit";
+import { useFetchListOfNames } from "./data/query";
+import { addWeight, updataWeight } from "./data/weightSlice";
+import { keyboardShortcut, keyDownDataSelect } from "./keyboardShortcut";
 
 const Entry = () => {
   const entryData = useAppSelector((state) => state.entryReducer)
   const weight = useAppSelector((state) => state.weightReducer.weight)
-  const weights = useAppSelector((state) => state.weightReducer)
   const formRef = useRef<HTMLFormElement>(null);
   const dispatch = useAppDispatch();
   const [date, setDate] = React.useState<Date>();
@@ -46,7 +45,6 @@ const Entry = () => {
     setDate(e)
     dispatch(updateEntry({ name: fieldName, value: e.toString() }))
   }
-
 
   const doNothingEnter = (e: { key: string; preventDefault: () => void; }) => {
     if (e.key === 'Enter') {
@@ -83,6 +81,8 @@ const Entry = () => {
     freightRate: false,
     otherCharge: false,
     labourRate: false,
+    freightKg: false,
+    labourKg: false,
   });
 
   const handleFocus = (name: string) => {
@@ -103,213 +103,199 @@ const Entry = () => {
       [name]: false,
     }));
   };
-  const { data, isError, isLoading, refetch } = useFetchListOfNames();
+  const { data, isError, isLoading } = useFetchListOfNames();
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (isError) {
     return <div>Error loading data</div>;
   }
-  const allInput = Array.from(document.querySelectorAll("[data-key='tab']")) as [HTMLElement]
-  allInput.forEach((input, index) => {
-    input.addEventListener('keydown', (e) => {
-      if (e.key == 'Enter') {
-        if (allInput.length > index + 1) {
-          allInput[index + 1].focus();
-        } else {
-          allInput[0].focus();
-        }
-      }
-    })
-  })
-
-
+  keyboardShortcut()
 
   return (
     <>
       <form ref={formRef} onSubmit={formSubmit} className="p-6" >
         <div className="flex p-6 flex-wrap border-gray-600 bg-white border-8 gap-10 m-auto justify-center">
-        {formData.map(
-          (field) =>
-            field.fieldType === fieldType.input ? (
-              <>
+          {formData.map(
+            (field) =>
+              field.fieldType === fieldType.input ? (
+                <>
+                  <div className="flex flex-col gap-1">
+                    <Label>{field.name}</Label>
+                    <Input
+                      data-key='tab'
+                      autoFocus={field.autofocus}
+                      onKeyDown={doNothingEnter}
+                      key={field.name}
+                      name={field.name}
+                      type={field.type}
+                      className={`w-fit ${field.className}`}
+                      placeholder={field.placeholder}
+                      value={entryData[field.name]}
+                      onChange={(e) => dispatch(updateEntry({ name: field.name, value: e.target.value }))}
+                      required
+                      data-autofocus='true'
+                      onFocus={(e) => e.target.select()}
+                      data-input={field.dataKey}
+                    />
+                  </div>
+                </>
+              ) : field.fieldType === fieldType.commandInput ? (
                 <div className="flex flex-col gap-1">
                   <Label>{field.name}</Label>
-                  <Input
-                    data-key='tab'
-                    autoFocus={field.autofocus}
-                    onKeyDown={doNothingEnter}
+                  <Command
+                    className="rounded-lg border w-fit shadow-md relative overflow-visible"
+                  >
+                    <CommandInput
+                      data-key='tab'
+                      className={`${field.className}`}
+                      placeholder="Type ..."
+                      onFocus={() => handleFocus(field.name)}
+                      onBlur={() => handleBlur(field.name)}
+                      value={entryData[field.name]}
+                      onValueChange={
+                        (e) => dispatch(
+                          updateEntry(
+                            {
+                              name: field.name,
+                              value: e
+                            }
+                          )
+                        )
+                      }
+                    />
+                    <CommandList className={`${isFocused[field.name] ? '' : 'hidden'} absolute w-full top-10 z-10 bg-white shadow-lg border`} >
+                      <CommandGroup  >
+                        {data?.[field.name]?.slice(0, 5)?.map((name: string, index: number) => (
+                          <CommandItem
+                            key={index}
+                            onSelect={(e) => {
+                              dispatch(updateEntry({ name: field.name, value: e }))
+                            }}
+                          >
+                            {name}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </div>
+              ) : field.fieldType === fieldType.selectInput ? (
+                <div className="flex flex-col gap-1">
+                  <Label>{field.name}</Label>
+                  <Select
+                    onValueChange={(value) => {
+                      dispatch(updateEntry({ name: field.name, value: value }));
+
+                    }
+                    }
                     key={field.name}
                     name={field.name}
-                    type={field.type}
-                    className={`w-fit ${field.className}`}
-                    placeholder={field.placeholder}
                     value={entryData[field.name]}
-                    onChange={(e) => dispatch(updateEntry({ name: field.name, value: e.target.value }))}
-                    required
-                    data-autofocus='true'
-                    onFocus={(e) => e.target.select()}
-                    data-select={field.dataKey}
-                  />
-                </div>
-              </>
-            ) : field.fieldType === fieldType.commandInput ? (
-              <div className="flex flex-col gap-1">
-                <Label>{field.name}</Label>
-                <Command
-                  className="rounded-lg border w-fit shadow-md relative overflow-visible"
-                >
-                  <CommandInput
-                    data-key='tab'
-                    className={`${field.className}`}
-                    placeholder="Type ..."
-                    onFocus={() => handleFocus(field.name)}
-                    onBlur={() => handleBlur(field.name)}
-                    value={entryData[field.name]}
-                    onValueChange={
-                      (e) => dispatch(
-                        updateEntry(
-                          {
-                            name: field.name,
-                            value: e
-                          }
-                        )
-                      )
-                    }
-                  />
-                  <CommandList className={`${isFocused[field.name] ? '' : 'hidden'} absolute w-full top-10 z-10 bg-white shadow-lg border`} >
-                    <CommandGroup  >
-                      {data?.[field.name]?.slice(0, 5)?.map((name: string, index: number) => (
-                        <CommandItem
-                          key={index}
-                          onSelect={(e) => {
-                            dispatch(updateEntry({ name: field.name, value: e }))
-                          }}
-                        >
-                          {name}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </div>
-            ) : field.fieldType === fieldType.selectInput ? (
-              <div className="flex flex-col gap-1">
-                <Label>{field.name}</Label>
-                <Select
-                  onValueChange={(value) =>{
-                    dispatch(updateEntry({ name: field.name, value: value }));
-
-                  }
-                  }
-                  key={field.name}
-                  name={field.name}
-                  value={entryData[field.name]}
-                >
-                  <SelectTrigger
-                    data-key='tab'
-                    className="w-20">
-                    <SelectValue
-                      placeholder="Select an option" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="peti">Peti</SelectItem>
-                    <SelectItem value="daba">Daba</SelectItem>
-                    <SelectItem value="box">Box</SelectItem>
-                    <SelectItem value="plate">Plate</SelectItem>
-                    <SelectItem value="charat1">Charat1</SelectItem>
-                    <SelectItem value="charat2">Charat2</SelectItem>
-                    <SelectItem value="charat3">Charat3</SelectItem>
-                    <SelectItem value="charat4">Charat3</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            ) : field.fieldType === fieldType.datePicker ? (
-              <div className="flex flex-col gap-1">
-                <Label>{field.name}</Label>
-                <Popover>
-                  <PopoverTrigger
-                    data-key='tab'
-                    onKeyDown={doNothingEnter}
-                    asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? (
-                        format(date, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={(e) => { e && setNewDate(e, field.name) }}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-              </div>
-            ) : field.fieldType === fieldType.weightInput ? (
-              <div className='w-full flex gap-4 justify-center' >
-                <div className="w-[750px] flex flex-wrap gap-4 justify-center">
-                  {weight.map((w: string, index: number) => (
-                    <>
-                      <Input
-                        onKeyDown={doNothingEnter}
-                        className="w-32"
-                        key={index}
-                        data-key='tab'
-                        value={weight[index]}
-                        type="number"
-                        onChange={(e) => dispatch(updataWeight({ index: index, value: e.target.value }))}
-                        onFocus={(e) => e.target.select()}
-                      />
-                    </>
-                  ))
-                  }
-                </div>
-                <Button onClick={handleAddWeight} >Add Weigth</Button>
-              </div>
-            ) : field.fieldType === fieldType.selectInputKg ? (
-              <div className="flex flex-col gap-1">
-                <Label>{field.name}</Label>
-                <Select
-                  onValueChange={(value) =>
-                    dispatch(updateEntry({ name: field.name, value: value }))
-                  }
-                  key={field.name}
-                  name={field.name}
-                >
-                  <SelectTrigger 
-                    data-key='tab'
-                      className="w-20"
                   >
-                    <SelectValue
+                    <SelectTrigger
                       data-key='tab'
-                      placeholder="Select an option" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="true">Kg</SelectItem>
-                    <SelectItem value="false">Nug</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            ) : null
-        )}
-        <Button
-          data-key='tab'
-          type="submit">Save</Button>
+                      data-type='type'
+                      className="w-20">
+                      <SelectValue
+                        placeholder="Select an option" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} data-type='okay' value="peti">Peti</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="daba">Daba</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="box">Box</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="plate">Plate</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="charat1">Charat1</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="charat2">Charat2</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="charat3">Charat3</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={NextSelect.typeItem} value="charat4">Charat3</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              ) : field.fieldType === fieldType.datePicker ? (
+                <div className="flex flex-col gap-1">
+                  <Label>{field.name}</Label>
+                  <Popover>
+                    <PopoverTrigger
+                      data-key='tab'
+                      onKeyDown={doNothingEnter}
+                      asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[280px] justify-start text-left font-normal",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {date ? (
+                          format(date, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={(e) => { e && setNewDate(e, field.name) }}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              ) : field.fieldType === fieldType.weightInput ? (
+                <div className='w-full flex gap-4 justify-center' >
+                  <div className="w-[750px] flex flex-wrap gap-4 justify-center">
+                    {weight.map((_: string, index: number) => (
+                      <>
+                        <Input
+                          onKeyDown={doNothingEnter}
+                          className="w-32"
+                          key={index}
+                          data-key='tab'
+                          value={weight[index]}
+                          type="number"
+                          onChange={(e) => dispatch(updataWeight({ index: index, value: e.target.value }))}
+                          onFocus={(e) => e.target.select()}
+                        />
+                      </>
+                    ))
+                    }
+                  </div>
+                  <Button onClick={handleAddWeight} >Add Weigth</Button>
+                </div>
+              ) : field.fieldType === fieldType.selectInputKg ? (
+                <div className="flex flex-col gap-1">
+                  <Label>{field.name}</Label>
+                  <Select
+                    onValueChange={(value) => dispatch(updateEntry({ name: field.name, value: value }))}
+                    key={field.name}
+                    name={field.name}
+                  >
+                    <SelectTrigger
+                      data-key='tab'
+                      className="w-20"
+                    >
+                      <SelectValue
+                        data-key='tab'
+                        placeholder="Select an option" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={field.dataKey} value="true">Kg</SelectItem>
+                      <SelectItem onKeyDown={(e) => keyDownDataSelect(e)} data-item={field.dataKey} value="false">Nug</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              ) : null
+          )}
+          <Button
+            data-key='tab'
+            type="submit">Save</Button>
 
         </div>
-      </form>
+      </form >
     </>
   );
 };
